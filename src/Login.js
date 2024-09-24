bf7fa80fdf1e2f6a2bd8cbc9b03661a9249aaa6f
hp <niranjan962796@gmail.com>
2024-09-24 18:42:22 +0530

    sisth comit

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,78 @@
 import "./Login.css";
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import { useState } from "react";
+// import PhoneInput from 'react-phone-number-input/input'
+import axios from "axios"
 
 
 
 
 
 const Login=({outLoginPage})=>{
+    const [value, setValue] = useState("91+")
+    const [clik , setClick] = useState(true)
+    const [mobile , setMobile] = useState("");
+    const [password , setPassword] = useState("");
+    const handleLoginInpurt=(e)=>{
+        setClick(false)
+    }
+
+    
+    const handleRegisterInpurt=()=>{
+        setClick(true)
+    }
+const handleMobile=(e)=>{
+    setMobile(e.target.value)
+}
+const handlePassword=(e)=>{
+    setPassword(e.target.value)
+}
+const outLoginPage2=(e)=>{
+    // e.preventDefault()
+    const user = {mobile,password};
+    console.log(user);
+    axios.post("https://hellostay.com/api/auth/login",{user})
+    .then(res=>{
+        console.log(res)
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+}
+
+
     return (
         <>
         <div className="login"></div>
         <div className="login-1">
            <div className="login-2">
-            <button>Login</button>
-            <button>Register</button>
+            <button onClick={handleLoginInpurt}>Login</button>
+            <button onClick={handleRegisterInpurt}>Register</button>
            </div>
            <div className="login-3">
-           <input type="text" placeholder="Mobile Number"></input>
-           <input type="text" placeholder="Pasword"></input>
-           <button onClick={outLoginPage}>Login</button>
+            
+                {
+                    clik ? (<div><input onChange={handleMobile} value={mobile} type="text" placeholder="Mobile Number"></input>
+                        <input value={password} onChange={handlePassword} type="text" placeholder="Pasword"></input>
+                        <button onClick={()=>{
+                            outLoginPage()
+                            outLoginPage2()
+                        }}>Login</button></div>) 
+                        :
+                        (<div><PhoneInput type="text" country="India"  placeholder="Enter Mobile Number" value={value}></PhoneInput>
+                            <button onClick={outLoginPage}>Send OTP</button></div>)
+                }
+                 
+
+
+                
+           
+            
+           
+
+
+
            </div>
         </div>
         </>
